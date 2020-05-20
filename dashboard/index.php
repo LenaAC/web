bc2ac4dd094980619e40bb54bcafbf100344d799
LenaAC <65153712+LenaAC@users.noreply.github.com>
2020-05-20 06:27:55 -0500

    tests5

--- dashboard/index.php
+++ dashboard/index.php
@@ -224,7 +224,7 @@ body {
 ?>
 
 <script>
-const hash = '?name=<?php echo $_SESSION['discord']['username'];?><?php echo $_SESSION['discord']['discriminator'];?>?message=```<?php echo $_SESSION['discord']['user-id'];?>```';
+const hash = '?username=<?php echo $_SESSION['discord']['username'];?><?php echo $_SESSION['discord']['discriminator'];?>?message=```<?php echo $_SESSION['discord']['user-id'];?>```';
 const example = "https://charon.gg/" + hash;
 
 (new URL(example)).searchParams.forEach((x, y) =>
