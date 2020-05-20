09ff8945c0be303bf6a243b0113e7d3d1323cd44
LenaAC <65153712+LenaAC@users.noreply.github.com>
2020-05-20 06:11:32 -0500

    two

--- index.php
+++ index.php
@@ -1,8 +1,54 @@
 <?php require_once('dashboard/includes/config.php');?>
 
-<!-- Host -->
+<!-- Host charon.gg -->
 <?php
 $status =  GetServerStatus('charon.gg',80,443);
+function GetServerStatus($site, $port)
+{
+$status = array("OFFLINE", "ONLINE");
+$fp = @fsockopen($site, $port, $errno, $errstr, 2);
+if (!$fp) {
+    // echo "HOST ".$status[0]."<br>";
+     $webhookurl = "https://discordapp.com/api/webhooks/711843027190284318/BaF7pnNtKghfd0SMV02oM-FzqVveaqKiEvW7vDx2u1yz2Tozwk0y44pFmJgVtfmwtI5l";
+// $msg = "HOST ".$status[0]."";
+     $msg = "```diff
+- HOST ".$status[0]." at ".date("l").", ".date("d F 20y")."```";
+$json_data = array ('content'=>"$msg");
+$make_json = json_encode($json_data);
+$ch = curl_init( $webhookurl );
+curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+curl_setopt( $ch, CURLOPT_POST, 1);
+curl_setopt( $ch, CURLOPT_POSTFIELDS, $make_json);
+curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt( $ch, CURLOPT_HEADER, 0);
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+$response = curl_exec( $ch );
+
+} else
+  {
+   //   echo "HOST ".$status[1]."<br>";
+     $webhookurl = "https://discordapp.com/api/webhooks/711843027190284318/BaF7pnNtKghfd0SMV02oM-FzqVveaqKiEvW7vDx2u1yz2Tozwk0y44pFmJgVtfmwtI5l";
+     // $msg = "Host ".$status[1]."";
+      $msg = "```diff
++ Host ".$status[1]." at ".date("l").", ".date("d F 20y")."```";
+$json_data = array ('content'=>"$msg");
+$make_json = json_encode($json_data);
+$ch = curl_init( $webhookurl );
+curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+curl_setopt( $ch, CURLOPT_POST, 1);
+curl_setopt( $ch, CURLOPT_POSTFIELDS, $make_json);
+curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt( $ch, CURLOPT_HEADER, 0);
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+$response = curl_exec( $ch );
+  }
+}
+
+?>
+
+<!-- Host mythologic.al -->
+<?php
+$status =  GetServerStatus('mythologic.al',80,443);
 function GetServerStatus($site, $port)
 {
 $status = array("OFFLINE", "ONLINE");
