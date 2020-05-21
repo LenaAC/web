7f3545e08b375ee7bb7b2be9328d601f671492da
LenaAC <65153712+LenaAC@users.noreply.github.com>
2020-05-21 06:34:42 -0500

    revert

--- dashboard/index.php
+++ dashboard/index.php
@@ -96,7 +96,7 @@ function Create_Random_Code($Length = 8, $IN_Params = [])
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <script src='https://kit.fontawesome.com/a076d05399.js'></script>
   <link href="../favicon.png" rel="icon" type="images/png">
-  <link href="css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+  <link href="bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
   <link href="css/style.css" rel="stylesheet" />
   <link rel="stylesheet" href="css/main.css"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
