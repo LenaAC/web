6ce1f375fb9cd00bb58e1404082f6f5c9cc5a4b1
LenaAC <65153712+LenaAC@users.noreply.github.com>
2020-05-21 06:50:47 -0500

    aaaa

--- dashboard/admin.php
+++ dashboard/admin.php
@@ -22,15 +22,21 @@
   <!DOCTYPE html>
   <html>
   <head>
-  	<title>Charon | Donate</title>
+  	<title>Charon | Admin</title>
+    <meta content="admin@charon.gg" name="description">
+    <meta content="charon" name="keywords">
   	<meta content="1000;https://charon.gg/" http-equiv="refresh">
   	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
   	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
   	<link href="../bootstrap.min.css" rel="stylesheet">
-  	<meta content="admin@charon.gg" name="description">
-  	<meta content="charon" name="keywords">
-  	<link href="../favicon.png" rel="icon" type="images/png">
   	<link href="css/style.css" rel="stylesheet">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
   	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
 
           <script type="text/javascript">
